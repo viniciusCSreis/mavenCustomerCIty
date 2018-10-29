87401a349ae5ecf71b136f69319f178004ba2f4a
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-29 11:39:00 -0300

    quick start maven

---
+++ mavenCustomerClient/src/main/java/br/zup/customerClient/App.java
@@ -0,0 +1,13 @@
+package br.zup.customerClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
